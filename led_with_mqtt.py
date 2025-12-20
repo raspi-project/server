6b545d9914265ff7b988d6faf8bc4a9026a20f7f
sirviumesh100 <umesirvi500@gmail.com>
2025-12-20 12:33:00 +0530

    Update led_with_mqtt.py

--- led_with_mqtt.py
+++ led_with_mqtt.py
@@ -8,6 +8,8 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 GPIO.output(LED_PIN, False)
 
+led_state = "OFF"
+
 # ---------------- MQTT ----------------
 BROKER = "YOUR_HIVEMQ_HOST"
 PORT = 8883
@@ -18,23 +20,27 @@ TOPIC_CONTROL = "umesh/led/control"
 TOPIC_STATUS  = "umesh/led/status"
 
 def on_connect(client, userdata, flags, rc):
+    print("MQTT connect result code:", rc)
     if rc == 0:
         print("Connected to MQTT Broker")
         client.subscribe(TOPIC_CONTROL)
-    else:
-        print("Connection failed")
+        client.publish(TOPIC_STATUS, led_state)
 
 def on_message(client, userdata, msg):
-    message = msg.payload.decode()
-    print("Received:", message)
+    global led_state
+    command = msg.payload.decode()
+    print("Received:", command)
 
-    if message == "ON":
+    if command == "ON" and led_state == "OFF":
         GPIO.output(LED_PIN, True)
+        led_state = "ON"
         client.publish(TOPIC_STATUS, "ON")
 
-    elif message == "OFF":
+    elif command == "OFF" and led_state == "ON":
         GPIO.output(LED_PIN, False)
+        led_state = "OFF"
         client.publish(TOPIC_STATUS, "OFF")
+
 try:
     client = mqtt.Client()
     client.username_pw_set(USERNAME, PASSWORD)
@@ -46,8 +52,10 @@ try:
     print("Connecting to broker...")
     client.connect(BROKER, PORT, 60)
     client.loop_forever()
+
 except KeyboardInterrupt:
-    print("KeyBoardInterrupt...")
+    print("KeyboardInterrupt...")
+
 finally:
     GPIO.cleanup()
     print("Exiting from System")
